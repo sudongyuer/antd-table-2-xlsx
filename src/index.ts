5a06324bdfc12d27b4ae1c8855d11b2ecc6123a2
sudongyu <976499226@qq.com>
2022-05-13 15:24:11 +0800

    feat: add index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-export const one = 1
-export const two = 2
+import type { ColumnsType } from 'antd/lib/table/interface'
+import * as ExcelJs from 'exceljs'
+import { generateHeaders, saveWorkbook } from './utils'
+function useExcel() {
+  function antdTable2xlsx(columns: ColumnsType, data: object[], fileName: string) {
+    try {
+      // 创建工作簿
+      const workbook = new ExcelJs.Workbook()
+      // 添加sheet
+      const worksheet = workbook.addWorksheet('sheet')
+      // 设置 sheet 的默认行高
+      worksheet.properties.defaultRowHeight = 20
+      // 设置列
+      worksheet.columns = generateHeaders(columns)
+      // 添加行
+      worksheet.addRows(data)
+      // 导出excel
+      saveWorkbook(workbook, `${fileName}.xlsx`)
+    }
+    catch (err) {
+      console.warn(err)
+    }
+  }
+  return {
+    antdTable2xlsx,
+  }
+}
+
+export { useExcel }
