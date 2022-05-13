2554065eae41631b2fdb49ec457e0cb5ea5db67c
sudongyu <976499226@qq.com>
2022-05-13 15:24:26 +0800

    feat: add types

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export interface ITableHeader {
+  header: string
+  // 用于数据匹配的 key
+  key: string
+  // 列宽
+  width: number
+  // 父级的 key
+  parentKey?: string
+  children?: ITableHeader[]
+}
+
+export interface IStyleAttr {
+  color?: string
+  fontSize?: number
+  horizontal?: 'fill' | 'distributed' | 'justify' | 'center' | 'left' | 'right' | 'centerContinuous' | undefined
+  bold?: boolean
+}
